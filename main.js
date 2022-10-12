59510d134823717d3ee4fbc9224146040b782146
Leonard_Sausa <leonardsausa.accnts@gmail.com>
2022-10-12 19:31:38 +0800

    new

--- main.js
+++ main.js
@@ -27,17 +27,36 @@
 
 
 
-const myFunction = function() {
-  let num1 = document.getElementById("input1").value;
-  let num2 = document.getElementById("input2").value;
-  //let result = Number(num1) + Number (num2);
-  let result = Number(num1) * (9/5) + 32;
-  let result_final = parsefloat(result).toFixed(2);
-  return result;
+// const myFunction = function() {
+//   let num1 = document.getElementById("input1").value;
+//   let num2 = document.getElementById("input2").value;
+//   //let result = Number(num1) + Number (num2);
+//   let result = Number(num1) * (9/5) + 32;
+//   let result_final = parsefloat(result).toFixed(2);
+//   return result;
+// };
+
+// const clearField = function() {
+//   document.getElementById("input1").value = "";
+//   document.getElementById("input2").value = "";
+
+// };
+
+
+const myFunction = function(){
+  let num1 = document.getElementById("cel").value;
+  let result  = Number(num1)  * Number(9/5) + Number(32); //parse
+ return result;
 };
 
-const clearField = function() {
-  document.getElementById("input1").value = "";
-  document.getElementById("input2").value = "";
+//additional
+let result_final = parseFloat(result).toFixed(2);
 
-};
+//another (onkeyup)
+Celcius <input type="text" id="cel" name="cel" onkeydown="document.getElementById('fah').value = (myFunction());">
+
+const ClearFields = function () {
+
+  document.getElementById("cel").value = "";
+  document.getElementById("fah").value = "";
+};  
